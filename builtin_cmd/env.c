897f3e6d99165093ef3316339fa9396728d9a750
Emir Ismail Genç <emgenc@student.42istanbul.com.tr>
2025-03-15 18:03:05 +0300

    "env" prototip

--- builtin_cmd/env.c
+++ builtin_cmd/env.c
@@ -6,7 +6,7 @@
 /*   By: emgenc <emgenc@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 12:45:02 by emgenc            #+#    #+#             */
-/*   Updated: 2025/03/15 13:15:26 by emgenc           ###   ########.fr       */
+/*   Updated: 2025/03/15 18:02:51 by emgenc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,26 @@
 #include "../main/minishell.h"
 #include "../t_split_utils/t_split.h"
 
-char*	env(char **envp)
+char	*env(char **envp)
 {
+	/* THIS IS FOR PRINTING ALL ENVIRONMENT VARIABLES "ONLY" !!! for now.
+	#include <unistd.h>
+	#include "../main/minishell.h"
+	#include "../t_split_utils/t_split.h"
+	
+	void	envdir(char **envp)
+	{
+		char **traversal;
+	
+		traversal = envp;
+		while (*traversal != NULL)
+			printf("%s\n", *(traversal++));
+	}
 
+	int main(int argc, char *argv[], char **envp) {
+		(void)argc;
+		(void)argv;
+		envdir(envp);
+	}
+	*/
 }
