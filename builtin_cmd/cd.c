e7e2775ba654604a44f25cfbbc32e4ead9515c74
Emir İsmail GENÇ <62707230+thecloudrazor@users.noreply.github.com>
2025-03-16 14:46:26 +0300

    Small modifications to "cd"

--- builtin_cmd/cd.c
+++ builtin_cmd/cd.c
@@ -15,6 +15,71 @@
 #include "../main/minishell.h"
 #include "../t_split_utils/t_split.h"
 
+static int	modifyenv(char **iterate, char *envvar, char *value)
+{
+	char	*initial_iterate;
+	char	*first_part;
+
+	initial_iterate = *iterate;
+	first_part = ft_strjoin(envvar, "=");
+	*iterate = ft_strjoin(first_part, value);
+	free(first_part);
+	free(initial_iterate);
+	return (0);
+}
+
+static int	realloc_createenv(char ***env, unsigned long long envamount, char *newent)
+{
+	char				**new_env;
+	unsigned long long	i;
+
+	/* Allocate space for (envamount + 1 new variable + 1 NULL terminator) pointers */
+	new_env = malloc(sizeof(char *) * (envamount + 2));
+	if (!new_env)
+		return (-1);
+	/* Copy the existing environment pointers PLUS the new environment variable, and terminate the array. */
+	i = 0;
+	while (i < envamount)
+		new_env[i] = (*env)[i++];
+	new_env[envamount] = newent;
+	new_env[envamount + 1] = NULL;
+
+	/* Free the old array and assign the pointer to the new one*/
+	free(*env);
+	*env = new_env;
+	return (0);
+}
+
+static int	e_setenv(char *envvar, char *value, char **allvars)
+{
+	char				**iterate;
+	unsigned long long	envamount;
+	char				*completed_envvar;
+	char				*first_part1;
+
+	iterate = allvars;
+	envamount = 0;
+	while (*iterate != NULL)
+	{
+		envamount++;
+		iterate++;
+	}
+	iterate = allvars;
+	while (*iterate != NULL
+		&& ft_strncmp(*iterate, envvar, ft_strlen(envvar)) != 0)
+		iterate++;
+	if (*iterate != NULL)
+		return(modifyenv(iterate, envvar, value), 0);
+	else if (ft_strncmp(*iterate, envvar, ft_strlen(envvar)) == 0)
+	{
+		first_part1 = ft_strjoin(envvar, "=");
+		completed_envvar = ft_strjoin(first_part1, value);
+		realloc_createenv(&allvars, envamount, completed_envvar);
+		return(free(first_part1), 0);
+	}
+    return (-1);
+}
+
 void	cd(char **envp)
 {
 	char	*target;
@@ -57,66 +122,5 @@ void	cd(char **envp)
 	free(newpwd);
 }
 
-static int	e_setenv(char *envvar, char *value, char **allvars)
-{
-	char				**iterate;
-	unsigned long long	envamount;
-	char				*completed_envvar;
-	char				*first_part1;
 
-	iterate = allvars;
-	envamount = 0;
-	while (*iterate != NULL)
-	{
-		envamount++;
-		iterate++;
-	}
-	iterate = allvars;
-	while (*iterate != NULL
-		&& ft_strncmp(*iterate, envvar, ft_strlen(envvar)) != 0)
-		iterate++;
-	if (*iterate != NULL)
-		modifyenv(iterate, envvar, value);
-	else
-	{
-		first_part1 = ft_strjoin(envvar, "=");
-		completed_envvar = ft_strjoin(first_part1, value);
-		realloc_createenv(&allvars, envamount, completed_envvar);
-		free(first_part1);
-	}
-}
 
-static int	modifyenv(char **iterate, char *envvar, char *value)
-{
-	char	*initial_iterate;
-	char	*first_part;
-
-	initial_iterate = *iterate;
-	first_part = ft_strjoin(envvar, "=");
-	*iterate = ft_strjoin(first_part, value);
-	free(first_part);
-	free(initial_iterate);
-	return (0);
-}
-
-static int	realloc_createenv(char ***env, unsigned long long envamount, char *newent)
-{
-	char				**new_env;
-	unsigned long long	i;
-
-	/* Allocate space for (envamount + 1 new variable + 1 NULL terminator) pointers */
-	new_env = malloc(sizeof(char *) * (envamount + 2));
-	if (!new_env)
-		return (-1);
- 	/* Copy the existing environment pointers PLUS the new environment variable, and terminate the array. */
-	i = 0;
-	while (i < envamount)
-		new_env[i] = (*env)[i++];
-	new_env[envamount] = newent;
-	new_env[envamount + 1] = NULL;
-
-	/* Free the old array and assign the pointer to the new one*/
-	free(*env);
-	*env = new_env;
-	return (0);
-}
