1a0e15728d8b2eb09f58b2f373b53151dd089d24
gm-infinite <kuzeyberk18@gmail.com>
2025-04-17 16:28:54 +0300

    added 2 new functions, count_dir_entries for counting how many entries in the given path, get_cur_dir_entries which creates a t_split struct containing every entry and returns it.

--- wildcard_handle/wildcard_handle.c
+++ wildcard_handle/wildcard_handle.c
@@ -13,23 +13,63 @@
 #include "../main/minishell.h"
 #include <dirent.h>
 
-char *wildcard_handle(char* str)
+int count_dir_entries(const char *directory)
 {
-	DIR *current_dir;
-	char hidden_flag;
-
-	current_dir = opendir(".");
-	if()
+	DIR *dir;
 	struct dirent *entry;
-	while ((entry = readdir(current_dir)) != NULL) {
-    printf("%s\n", entry->d_name);
+	int count;
+
+	dir = opendir(directory);
+	entry = readdir(dir);
+	count = 0;
+	while (entry != NULL) {
+		count++;
+		entry = readdir(dir);
 	}
-	closedir(current_dir);
+	closedir(dir);
+	return (count);
 }
 
-int main()
+t_split get_cur_dir_entries(const char *direc)
 {
-	char *print = wildcard_handle("");
-	printf("%s", print);
-	free(print);
+	t_split cur_dir_ent;
+	DIR *dir;
+	int i;
+	struct dirent *entry;
+
+	cur_dir_ent.size = count_dir_entries(direc);
+	cur_dir_ent.start = (char **)ft_calloc(cur_dir_ent.size, sizeof(char *));
+	dir = opendir(direc);
+	entry = readdir(dir);
+	i = 0;
+	while (entry != NULL) {
+		cur_dir_ent.start[i] = ft_strdup(entry->d_name);
+		i++;
+		entry = readdir(dir);
+	}
+	closedir(dir);
+	return (cur_dir_ent);
+}
+
+char *wildcard_handle(char* str)
+{
+	t_split cur_dir_entries;
+
+	cur_dir_entries = get_cur_dir_entries(".");
+}
+
+int main(int ac, char **av)
+{
+	
+	t_split test;
+
+	test = get_cur_dir_entries(av[1]);
+	int i = 0;
+	while (i < test.size)
+	{
+		printf("%s\n", test.start[i]);
+		i++;
+	}
+	free_split(&test);
+	
 }
