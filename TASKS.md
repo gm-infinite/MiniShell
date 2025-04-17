6f1b7c62422b205748a0eff39928fb09d0ed2cc5
gm-infinite <kuzeyberk18@gmail.com>
2025-04-17 22:14:42 +0300

    Adds wildcard filter creation
    -Implements the initial logic for creating a filter based on a wildcard string.
    also added a note in tasks md for wildcard handling. the filter creatin still need to handle wildcard in quotes.
    
    This splits the wildcard into substrings based on the '*' character
    to later match directory entries.

--- TASKS.md
+++ TASKS.md
@@ -148,9 +148,9 @@
     **Difficulty:** —
 
 - ## EXTRA NOTES
-  - april 15th
-    - when using wildcard with bash I discovered that if the string with wildcard doesnt end with one, it doesnt count as a wilcard
-  
+  - april 17
+    - I made many discoveries with wildcard and how it works. heres some examples:
+    - cat \*" "\* , here if you have a file with space as a character in their name, this command works if not it looks for a file named '\*" "\*' .
 
   - april 8th, 2025
     - sep_opt_arg.c must be revised to comply with non-bonus requirements of the project. (|| and && won't be checked).
--- wildcard_handle/wildcard_handle.c
+++ wildcard_handle/wildcard_handle.c
@@ -13,16 +13,17 @@
 #include "../main/minishell.h"
 #include <dirent.h>
 
-int count_dir_entries(const char *directory)
+int	count_dir_entries(const char *directory)
 {
-	DIR *dir;
-	struct dirent *entry;
-	int count;
+	DIR				*dir;
+	struct dirent	*entry;
+	int				count;
 
 	dir = opendir(directory);
 	entry = readdir(dir);
 	count = 0;
-	while (entry != NULL) {
+	while (entry != NULL)
+	{
 		count++;
 		entry = readdir(dir);
 	}
@@ -30,19 +31,20 @@ int count_dir_entries(const char *directory)
 	return (count);
 }
 
-t_split get_cur_dir_entries(const char *direc)
+t_split	get_cur_dir_entries(const char *direc)
 {
-	t_split cur_dir_ent;
-	DIR *dir;
-	int i;
-	struct dirent *entry;
+	t_split			cur_dir_ent;
+	DIR				*dir;
+	int				i;
+	struct dirent	*entry;
 
 	cur_dir_ent.size = count_dir_entries(direc);
 	cur_dir_ent.start = (char **)ft_calloc(cur_dir_ent.size, sizeof(char *));
 	dir = opendir(direc);
 	entry = readdir(dir);
 	i = 0;
-	while (entry != NULL) {
+	while (entry != NULL)
+	{
 		cur_dir_ent.start[i] = ft_strdup(entry->d_name);
 		i++;
 		entry = readdir(dir);
@@ -51,25 +53,50 @@ t_split get_cur_dir_entries(const char *direc)
 	return (cur_dir_ent);
 }
 
-char *wildcard_handle(char* str)
+t_split	create_filter(const char *wildcard)
 {
-	t_split cur_dir_entries;
+	t_split		filter;
+	int			i;
+	int			j;
+	int			k;
+	int			size;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	size = ft_strlen(wildcard);
+	filter.size = countchr_not_quote((char *)wildcard, '*') + 1;
+	filter.start = (char **)ft_calloc(filter.size, sizeof(char *));
+	while (i <= size && j <= size)
+	{
+		while (wildcard[j] != '\0' && wildcard[j] != '*')
+			j++;
+		if (i != j)
+			filter.start[k] = ft_substr(wildcard, i, j - i);
+		else
+			filter.start[k] = ft_strdup("");
+		i = j + 1;
+		j = i;
+		k++;
+	}
+	return (filter);
+}
+
+void	wildcard_handle(t_shell shell)
+{
+	t_split	cur_dir_entries;
 
 	cur_dir_entries = get_cur_dir_entries(".");
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	
-	t_split test;
-
-	test = get_cur_dir_entries(av[1]);
+	t_split test_filter = create_filter(av[1]);
 	int i = 0;
-	while (i < test.size)
+	while (i < test_filter.size)
 	{
-		printf("%s\n", test.start[i]);
+		printf("%s\n", test_filter.start[i]);
 		i++;
 	}
-	free_split(&test);
-	
+	free_split(&test_filter);
 }
